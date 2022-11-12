6c544734ff4ee5236486797ab91e7c153ad621b6
unknown <winithewind@gmail.com>
2022-11-12 18:22:33 +1300

    redone object placement

--- 2DCafeSimProject/Assets/Scripts/InstantiateItems.cs
+++ 2DCafeSimProject/Assets/Scripts/InstantiateItems.cs
@@ -10,26 +10,35 @@ public class InstantiateItems : MonoBehaviour
 {
     [SerializeField] public GameObject deskToSpawn;
     [SerializeField] public GameObject tableToSpawn;
+    [SerializeField] public GameObject chairToSpawn;
+    [SerializeField] public GameObject metalDeskToSpawn;
 
     public GameObject prefabTransform;
 
     bool hasPurchased = false;
+    bool hasBeenPlaced = false;
 
     Tilemap map;
 
-    List<ItemsData> purchaseItemList = new List<ItemsData>();
-
-    int time = 0;
     public struct ItemsData
     {
         public string name;
         public int quantity;
     }
 
+    int counter = 0;
+    int limit = 6;
+
     public static List<ShopManager.ItemsData> itemData;
 
     GameObject panel;
     GameObject closeShopButton;
+
+    GameObject objPointer;
+
+    bool isCreating = false;
+
+    int increment = 0;
 
 
     void Start()
@@ -40,66 +49,169 @@ public class InstantiateItems : MonoBehaviour
     }
     public void GetPurchasedItems(object sender, ShopManager.PurchaseItemsEventArgs e)
     {
-        StartCoroutine(InstantiateGameObjectsSequence(e.itemData));
-    }
-    GameObject InstantiateDesk(GameObject objectToSpawn, Transform position, Quaternion quaternion)
-    {
-        return prefabTransform;
-    }
+        itemData = e.itemData;
 
-    IEnumerator InstantiateGameObjectsSequence(List<ShopManager.ItemsData> itemsData)
-    {
-        panel.SetActive(false);
-        foreach (var item in itemsData)
+        if (itemData.Count != 0)
         {
-            yield return StartCoroutine(PlaceObject(item.name, item.quantity, itemsData));
-            yield return null;
-        }
+            isCreating = true;
 
-        itemsData.Clear();
-        Debug.Log(itemsData.Count);
-        if (itemsData.Count == 0)
-        {
-            panel.SetActive(true);
         }
     }
 
-    IEnumerator PlaceObject(string name, int quantity, List<ShopManager.ItemsData> itemsData)
+    private void Update()
     {
-        if (name == "Desk")
+        if (isCreating == true)
         {
-            yield return StartCoroutine(SpawnObject(deskToSpawn, name, quantity, itemsData));
-            yield return null;
-        }
-        else if (name == "Table")
-        {
-            yield return StartCoroutine(SpawnObject(tableToSpawn, name, quantity, itemsData));
-            yield return null;
+            StartCreation(itemData);
+            this.panel.SetActive(false);
+        } else {
+            this.panel.SetActive(true);
         }
     }
 
-    IEnumerator SpawnObject(GameObject objectToSpawn, string name, int quantity, List<ShopManager.ItemsData> itemsData)
+    GameObject obj = null;
+    void StartCreation(List<ShopManager.ItemsData> itemData)
     {
-        GameObject obj = Instantiate(objectToSpawn, transform.position, Quaternion.identity);
-        var counter = 0;
-        while (counter < quantity)
+        if (itemData.Count != 0)
+        {
+            if (itemData[0].name == "Desk")
+            {
+                if (obj == null)
+                {
+                    obj = Instantiate(deskToSpawn, transform.position, Quaternion.identity);
+                    hasBeenPlaced = false;
+                }
+            }
+            else if (itemData[0].name == "Table")
+            {
+                if (obj == null)
+                {
+                    obj = Instantiate(tableToSpawn, transform.position, Quaternion.identity);
+                    hasBeenPlaced = false;
+                }
+            } else if (itemData[0].name == "Chair")
+            {
+                if (obj == null)
+                {
+                    obj = Instantiate(chairToSpawn, transform.position, Quaternion.identity);
+                    hasBeenPlaced = false;
+                }
+            } else if (itemData[0].name == "Metal Desk")
+            {
+                if (obj == null)
+                {
+                    obj = Instantiate(metalDeskToSpawn, transform.position, Quaternion.identity);
+                    hasBeenPlaced = false;
+                }
+            } 
+        } else {
+            isCreating = false;
+        }
+
+        if (hasBeenPlaced == false)
         {
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
             Vector3Int vec = map.WorldToCell(new Vector3(mousePosition.x, mousePosition.y, 0));
             Vector3 vec2 = map.GetCellCenterWorld(vec);
-            obj.transform.position = vec2;
 
-            if (Mouse.current.leftButton.wasPressedThisFrame == true)
+            if (obj != null)
             {
-                counter++;
-                Instantiate(objectToSpawn, vec2, Quaternion.identity);
+                obj.transform.position = vec2;
 
             }
-            yield return null;
         }
-        if (counter == quantity)
+
+        if (Mouse.current.leftButton.wasPressedThisFrame == true)
         {
-            obj.SetActive(false);
+            counter = counter + 1;
+
+            if (itemData.Count > 0)
+            {
+                PlaceObject(itemData[0].name, itemData[0].quantity, itemData);
+            }
         }
     }
+
+    void PlaceObject(string name, int quantity, List<ShopManager.ItemsData> itemData)
+    {
+        hasBeenPlaced = true;
+        obj = null;
+
+        if (counter >= quantity)
+        {
+            counter = 0;
+            itemData.Remove(itemData[0]);
+        }
+    }
+
+    // void InstantiateObject(GameObject objToSpawn)
+    // {
+    //     GameObject obj = Instantiate(objToSpawn, transform.position, Quaternion.identity);
+    //     Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+    //     Vector3Int vec = map.WorldToCell(new Vector3(mousePosition.x, mousePosition.y, 0));
+    //     Vector3 vec2 = map.GetCellCenterWorld(vec);
+    //     obj.transform.position = vec2;
+
+    // }
+
+    // GameObject InstantiateDesk(GameObject objectToSpawn, Transform position, Quaternion quaternion)
+    // {
+    //     return prefabTransform;
+    // }
+
+    // IEnumerator InstantiateGameObjectsSequence(List<ShopManager.ItemsData> itemsData)
+    // {
+    //     panel.SetActive(false);
+    //     foreach (var item in itemsData)
+    //     {
+    //         yield return StartCoroutine(PlaceObject(item.name, item.quantity, itemsData));
+    //         yield return null;
+    //     }
+
+    //     itemsData.Clear();
+    //     Debug.Log(itemsData.Count);
+    //     if (itemsData.Count == 0)
+    //     {
+    //         panel.SetActive(true);
+    //     }
+    // }
+
+    // IEnumerator PlaceObject(string name, int quantity, List<ShopManager.ItemsData> itemsData)
+    // {
+    //     if (name == "Desk")
+    //     {
+    //         yield return StartCoroutine(SpawnObject(deskToSpawn, name, quantity, itemsData));
+    //         yield return null;
+    //     }
+    //     else if (name == "Table")
+    //     {
+    //         yield return StartCoroutine(SpawnObject(tableToSpawn, name, quantity, itemsData));
+    //         yield return null;
+    //     }
+    // }
+
+    // IEnumerator SpawnObject(GameObject objectToSpawn, string name, int quantity, List<ShopManager.ItemsData> itemsData)
+    // {
+    //     GameObject obj = Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+    //     var counter = 0;
+    //     while (counter < quantity)
+    //     {
+    //         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+    //         Vector3Int vec = map.WorldToCell(new Vector3(mousePosition.x, mousePosition.y, 0));
+    //         Vector3 vec2 = map.GetCellCenterWorld(vec);
+    //         obj.transform.position = vec2;
+
+    //         if (Mouse.current.leftButton.wasPressedThisFrame == true)
+    //         {
+    //             counter++;
+    //             Instantiate(objectToSpawn, vec2, Quaternion.identity);
+
+    //         }
+    //         yield return null;
+    //     }
+    //     if (counter == quantity)
+    //     {
+    //         obj.SetActive(false);
+    //     }
+    // }
 }
